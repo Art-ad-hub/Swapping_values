8582e098bb60c368ce8d34b239d35a8522936aaa
Artem <130403999+Art-ad-hub@users.noreply.github.com>
2023-04-11 22:48:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Петров";
+            string lastName = "Денис";
+
+            Console.WriteLine($"Ваше имя {name}, а ваша фамилия {lastName}.");
+            Console.WriteLine("О нет, похоже что ваши данные перепутанны, давайте исправим?");
+            Console.Write("Введите  ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите  вашу фамилию: ");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Теперь должно быть правильно!");
+            Console.WriteLine($"Ваше имя {name}, а фамилия {lastName}.");
+            Console.ReadKey();
+        }
+    }
+}
